5eaeb3ad2f72f2e2711dbdbfe11e5727678b5a7f
lw-chadschwab <99629554+lw-chadschwab@users.noreply.github.com>
2022-06-14 15:21:10 -0500

    Squash all lworks changes prior to v0.60.0 for easier rebasing in the future
    Add rebase-lworks script
    
    Add entity listener which logs transaction count)
    
    DEV-468 - send transactions to kafka topic - em cs (#18)
    
    Co-authored-by: Erik Muir <erikdmuir@gmail.com>
    
    DEV-568 - send record items to kafka; remove sending of transactions (#20)
    
    * DEV-568 - send record items to kafka; remove sending of transactions
    
    * DEV-568 - switch to stable proto version
    
    DEV-595 - fix documentation (#23)

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/parser/record/entity/sql/SqlEntityListener.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/parser/record/entity/sql/SqlEntityListener.java
@@ -90,7 +90,7 @@ import com.hedera.mirror.importer.repository.SidecarFileRepository;
 
 @Log4j2
 @Named
-@Order(0)
+@Order(1)
 @ConditionOnEntityRecordParser
 public class SqlEntityListener implements EntityListener, RecordStreamFileListener {
 
