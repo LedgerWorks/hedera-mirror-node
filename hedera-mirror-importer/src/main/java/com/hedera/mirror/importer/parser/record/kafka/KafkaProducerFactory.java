2f9f891b997c24c119bc979c44c3f1ad8baf46e0
lw-chadschwab <99629554+lw-chadschwab@users.noreply.github.com>
2022-06-14 15:21:10 -0500

    Squash all lworks changes prior to v0.60.0 for easier rebasing in the future
    Add rebase-lworks script
    
    Add entity listener which logs transaction count)
    
    DEV-468 - send transactions to kafka topic - em cs (#18)
    
    Co-authored-by: Erik Muir <erikdmuir@gmail.com>
    
    DEV-568 - send record items to kafka; remove sending of transactions (#20)
    
    * DEV-568 - send record items to kafka; remove sending of transactions
    
    * DEV-568 - switch to stable proto version
    
    DEV-595 - fix documentation (#23)

---
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/parser/record/kafka/KafkaProducerFactory.java
@@ -0,0 +1,54 @@
+package com.hedera.mirror.importer.parser.record.kafka;
+
+import com.google.common.collect.ImmutableMap;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaProducerFactory {
+    private final KafkaProperties properties;
+
+    @Bean
+    KafkaTemplate<String, byte[]> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    ProducerFactory<String, byte[]> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    ImmutableMap<String, Object> producerConfigs() {
+        ImmutableMap.Builder<String, Object> propertyBuilder = ImmutableMap.<String, Object>builder()
+                .put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers())
+                .put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
+                .put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class)
+                .put(ProducerConfig.ACKS_CONFIG, "all"); // may need set to "1" if throughput becomes an issue
+        String apiKey = properties.getProducerApiKey();
+        if (apiKey != null && !apiKey.isBlank())
+        {
+            propertyBuilder.put(AdminClientConfig.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SASL_SSL.name)
+                .put(SaslConfigs.SASL_MECHANISM, "PLAIN")
+                .put(SaslConfigs.SASL_JAAS_CONFIG, getSaslJaasConfig());
+        }
+        return propertyBuilder.build();
+    }
+
+    private String getSaslJaasConfig() {
+        return String.format(
+                "org.apache.kafka.common.security.plain.PlainLoginModule required username='%s' password='%s';",
+                properties.getProducerApiKey(), properties.getProducerApiKeySecret());
+    }
+}
